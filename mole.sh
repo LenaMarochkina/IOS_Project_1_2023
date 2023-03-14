25f4f000cb4f74ec6338f4a835b8ef783506e2e5
LenaMarochkina <marochkina.lena@gmail.com>
2023-03-14 20:17:43 +0100

    feat: adds argument parsing

--- mole.sh
+++ mole.sh
@@ -1,32 +1,55 @@
-#Arguments parsing
-while getopts "hg:m:b:a:" opt; do
-    case $opt in
-        h)
-            # Display help message
-            print_help
-            ;;
-        g)
-            # Handle the -g option
-            IFS=',' read -r -a groups <<< "$OPTARG"
-            echo "Groups: ${groups[*]}"
-            ;;
-        m)
-            # Handle the -m option
-            most_used=true
-            ;;
-        b)
-            # Handle the -b option
-            DATE_BEFORE=$OPTARG
-            ;;
-        a)
-            # Handle the -a option
-            DATE_AFTER=$OPTARG
-            ;;
-        \?)
-            # Invalid option
-            print_help
-            ;;
-    esac
+POSIXLY_CORRECT=yes
+
+DIRECTORY=$('pwd')
+FILE=""
+COMMAND="open"
+DATE_AFTER=""
+DATE_BEFORE=""
+MOST_USED=0
+groups=()
+
+# parse arguments
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+  -h | --help)
+    print_help
+    exit 0
+    ;;
+  -a)
+    DATE_AFTER="$2"
+    shift 2
+    ;;
+  -b)
+    DATE_BEFORE="$2"
+    shift 2
+    ;;
+  -g)
+    IFS=',' read -r -a groups <<<"$2"
+    echo "Groups: ${groups[*]}"
+    shift 2
+    ;;
+  -m)
+    MOST_USED=1
+    shift
+    ;;
+  *)
+    if [[ "$1" == "list" || "$1" == "secret-log" ]]; then
+      COMMAND="$1"
+      shift
+
+    # TODO: if file/directory doesn't exist, leave it to the file editor to handle
+    # if argument is a file
+    elif [ -f "$1" ]; then
+      FILE="$1"
+      shift
+
+    # if argument is a directory
+    elif [ -d "$1" ]; then
+      DIRECTORY="$1"
+      shift
+    fi
+    ;;
+  esac
 done
 
 #Date validation
